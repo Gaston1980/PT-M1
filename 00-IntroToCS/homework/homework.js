e2c8ff62213c110c98a18acc3c8da5ba0c84fec7
Gaston1980 <gastonfrissiones@yahoo.com.ar>
2022-04-28 18:16:46 -0300

    Homework-00 Done

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -1,13 +1,28 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+var resultado = 0
+var contador = num.length-1
+for( var i = 0; i < num.length; i++){
+  resultado += num[i] * Math.pow(2,contador)
+  contador = contador - 1
+}
+return resultado
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
+  let entero;
+  let resto;
+  let binario = [];
+  do{
+    entero = Math.floor(num / 2);
+    resto = num % 2;
+    binario.unshift(resto);
+    num = entero;
+  }
+  while((num / 2) > 0)
 
+  return binario.join("");
 }
 
 
